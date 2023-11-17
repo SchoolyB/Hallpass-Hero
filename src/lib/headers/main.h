d1d7fd099f5fd678f21aaeffd306d4a3079814c7
Schoolyb <marshallbcodes@gmail.com>
2023-11-17 10:30:09 -0600

    Work on main/add prototypes to main.h

--- src/lib/headers/main.h
+++ src/lib/headers/main.h
@@ -1,6 +1,13 @@
-#ifndef PROTOTYPES_H
-#define PROTOTYPES_H
+#ifndef MAIN_H
+#define MAIN_H
 
+// PROTOTYPES
 void refresh_main_menu();
-
+void handle_main_menu_decision();
+void create_new_roster();
+// void manage_existing_roster();
+// void add_student_to_database();
+// void manage_student_database();
+// void search_for_student();
+// void show_help_menu();
 #endif
