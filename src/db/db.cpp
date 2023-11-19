3c850a1e24380cf7ef76fc581cab144eaad4da56
Schoolyb <marshallbcodes@gmail.com>
2023-11-19 14:07:17 -0600

    Fix roster db creation bug and some error logging

--- src/db/db.cpp
+++ src/db/db.cpp
@@ -2,26 +2,66 @@
 #include <sqlite3.h>
 #include "../lib/utils.h"
 
+static int callback(void *NotUsed, int argc, char **argv, char **azColName)
+{
+  int i;
+  for (i = 0; i < argc; i++)
+  {
+    printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+  }
+  printf("\n");
+  return 0;
+}
+
 extern "C"
 {
-  int create_new_roster_table()
+
+  int create_new_roster_table(const char *rosterName)
   {
+
+    /*
+      This takes in a string when its called from a C source file
+      this line converts the C string into a C++ string
+    */
+    std::string cppString(rosterName);
+
+    // print what the C++ string got from the C source file
+    // TODO remove after debugging
+    std::cout << "The CPP function received string: " << rosterName << std::endl;
+
     sqlite3 *db;
     int rc = sqlite3_open("../build/db.sqlite", &db);
 
-    char *freeMessage;
-    // Add your SQLite operations here, if needed
-    // exit = sqlite3_exec(db, sql.c_str(), NULL, 0, &message);
+    if (rc != SQLITE_OK)
+    {
+      std::cerr << "Failed to open SQLite3 database: " << sqlite3_errmsg(db) << std::endl;
+      sqlite3_close(db);
+      return 1; // Indicate failure
+    }
+
+    char createTableSQL[200]; // Adjust the size accordingly
+
+    sprintf(createTableSQL, "CREATE TABLE IF NOT EXISTS %s ("
+                            "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                            "name TEXT NOT NULL,"
+                            "age INTEGER);",
+            rosterName);
+
+    // Uncomment the following line if you have a callback function defined
+    // rc = sqlite3_exec(db, createTableSQL, callback, nullptr, nullptr);
+
+    rc = sqlite3_exec(db, createTableSQL, nullptr, nullptr, nullptr); // No callback for now
 
     if (rc != SQLITE_OK)
     {
-      UTILS_ERROR_LOGGER("Error opening SQLite3 database:", "create_new_roster_table", "exit");
-      sqlite3_free(freeMessage);
+      std::cerr << "SQL error: " << sqlite3_errmsg(db) << std::endl;
+      sqlite3_close(db);
+      return 4; // Indicate failure
     }
-    else
-      std::cout << "Database opened Successfully" << std::endl;
+
+    std::cout << "Database opened successfully" << std::endl;
 
     sqlite3_close(db);
-    return 0;
+    return 0; // Indicate success
   }
 }
